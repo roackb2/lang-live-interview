d10c54d933f0218a59b2abd00247d723e24c1c31
Jay/Fienna Liang <roackb2@gmail.com>
2021-09-19 23:10:54 +0800

    remove redundant code

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter.backup/counterSlice';
 import resultReducer from '../features/result/resultSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     result: resultReducer
   },
 });
